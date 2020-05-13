ad4225c1d98eb26522cd2239945dbf4c9b52ae48
BrittonFischer <brittonfischerdev@gmail.com>
2020-05-12 23:12:35 -0700

    Updates

---
+++ precache-manifest.d924a66e1c618a93a3f39f7241157652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c182daf097eb9c852b00d10fe9396e20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91e78485b9ace05c82e9",
+    "url": "/monsters-rolodex/static/css/main.a2d5dc64.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e78485b9ace05c82e9",
+    "url": "/monsters-rolodex/static/js/main.d0ba8556.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
